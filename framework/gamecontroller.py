8b0925181b1e446db08d1000a2806e9cc75f33db
Matthew Henry <matthewhenry.info@gmail.com>
2023-10-03 16:53:36 -0400

    Working on war

--- framework/gamecontroller.py
+++ framework/gamecontroller.py
@@ -36,6 +36,8 @@ class GameController(object):
         self.collecting_step = 0
         self.game_winner = None
         self.paused = False
+        self.speed = 1
+        self.increase_speed = None
         pass
     def reset(self):
         self.deck = Deck()
@@ -65,35 +67,65 @@ class GameController(object):
         self.game_winner = None
     def act(self, events):
         for event in events:
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_r:
                     self.reset()
                 if event.key == pygame.K_p:
                     self.paused = not self.paused
+                if event.key == pygame.K_UP:
+                    if self.increase_speed and self.increase_speed < 3:
+                        self.increase_speed += 1
+                    if not self.increase_speed and self.speed < 3:
+                        self.increase_speed = self.speed + 1
+                if event.key == pygame.K_DOWN:
+                    if self.increase_speed and self.increase_speed > 1:
+                        self.increase_speed -= 1
+                    if not self.increase_speed and self.speed > 1:
+                        self.increase_speed = self.speed - 1 
         if not self.paused:
             if self.dealing:
-                deal_speed = 10
-                if len(self.deck.cards) > 0 and not self.dealing_card:
-                    self.dealing_card = self.deck.cards.pop()
-                elif self.dealing_card:
-                    if self.deal_step > deal_speed:
-                        self.player1_cards.append(self.dealing_card)
-                        self.deal_step = -1
-                        self.dealing_card = None
-                    if self.deal_step < -(deal_speed):
-                        self.player2_cards.append(self.dealing_card)
-                        self.deal_step = 1
+                if self.increase_speed:
+                    self.speed = self.increase_speed
+                    self.increase_speed = None
+                if self.speed == 3:
+                    if self.dealing_card:
+                        if len(self.player1_cards) <= len(self.player2_cards):
+                            self.player1_cards.append(self.dealing_card)
+                        else:
+                            self.player2_cards.append(self.dealing_card)
                         self.dealing_card = None
 
-                    if len(self.player1_cards) <= len(self.player2_cards) and self.deal_step > 0:
-                        self.deal_step += 1
-                    elif len(self.player1_cards) > len(self.player2_cards) and self.deal_step < 0:
-                        self.deal_step -= 1
-                else:
+                    while len(self.deck.cards) > 0:
+                        if len(self.player1_cards) <= len(self.player2_cards):
+                            self.player1_cards.append(self.deck.cards.pop())
+                        else:
+                            self.player2_cards.append(self.deck.cards.pop())
+
                     self.dealing = False
                     self.battle = True
+                else:
+                    deal_speed = 10
+                    if self.speed == 2:
+                        deal_speed = 1
+                    if len(self.deck.cards) > 0 and not self.dealing_card:
+                        self.dealing_card = self.deck.cards.pop()
+                    elif self.dealing_card:
+                        if self.deal_step > deal_speed:
+                            self.player1_cards.append(self.dealing_card)
+                            self.deal_step = -1
+                            self.dealing_card = None
+                        if self.deal_step < -(deal_speed):
+                            self.player2_cards.append(self.dealing_card)
+                            self.deal_step = 1
+                            self.dealing_card = None
+
+                        if len(self.player1_cards) <= len(self.player2_cards) and self.deal_step > 0:
+                            self.deal_step += 1
+                        elif len(self.player1_cards) > len(self.player2_cards) and self.deal_step < 0:
+                            self.deal_step -= 1
+                    else:
+                        self.dealing = False
+                        self.battle = True
 
             if self.battle:
                 if self.battle_step == 0:
@@ -109,7 +141,9 @@ class GameController(object):
                         self.player2_battle_cards.append(self.player2_cards.pop())
                 else:
                     self.battle_step += 1
-                    if self.battle_step == 20:
+                    if ((self.speed == 1 and self.battle_step == 20) 
+                        or (self.speed == 2 and self.battle_step == 10)
+                        or (self.speed == 3 and self.battle_step == 2)):
                         self.display_battle = True
                         if self.player1_battle_cards[0].value > self.player2_battle_cards[0].value:
                             self.battle_winner = 1
@@ -119,17 +153,24 @@ class GameController(object):
                             self.war = True
                             self.battle = False
                             self.battle_step = 0
-                    if self.battle_step > 150:
+                    if ((self.speed == 1 and self.battle_step > 150) 
+                        or (self.speed == 2 and self.battle_step > 60)
+                        or (self.speed == 3 and self.battle_step > 2)):
                         self.display_battle = False
                         self.collecting_step = 0
                         self.collecting = True
                         self.battle = False
                         self.war = False
                         self.battle_step = 0
+                        if self.increase_speed:
+                            self.speed = self.increase_speed
+                            self.increase_speed = None
 
             if self.collecting and self.collecting_step == 0:
                 self.collecting_step = 1
-            elif self.collecting and self.collecting_step < 40:
+            elif ((self.speed == 1 and self.collecting and self.collecting_step < 40)
+                or (self.speed == 2 and self.collecting and self.collecting_step < 15)
+                or (self.speed == 3 and self.collecting and self.collecting_step < 2)):
                 self.collecting_step += 1
             elif self.collecting:
                 if self.battle_winner == 1:
@@ -150,6 +191,9 @@ class GameController(object):
                 self.war = False
                 self.battle = True
                 self.battle_winner = None
+                if self.increase_speed:
+                    self.speed = self.increase_speed
+                    self.increase_speed = None
 
             if self.war:
                 if self.war_step == 0:
@@ -166,7 +210,9 @@ class GameController(object):
                             self.player2_drawing_war_cards.append(self.player2_cards.pop())
                 else:
                     self.war_step += 1
-                    if self.war_step == 20:
+                    if ((self.speed == 1 and self.war_step == 20)
+                       or (self.speed == 2 and self.war_step == 10)
+                       or (self.speed == 3 and self.war_step == 2)):
                         for x in range(3):
                             self.player1_war_cards.append(self.player1_drawing_war_cards.pop())
                             self.player2_war_cards.append(self.player2_drawing_war_cards.pop())
@@ -179,19 +225,25 @@ class GameController(object):
                             self.battle_winner = 2
                         else:
                             self.war_again = True
-                    if self.war_step > 150:
-                        if self.war_again:
-                            self.war_step = 0
-                            self.war_again = False
-                        else:
-                            self.display_battle = False
-                            self.dislay_war = False
-                            self.collecting_step = 0
-                            self.collecting = True
-                            self.battle = False
-                            self.battle_step = 0
-                            self.war = False
-                            self.war_step = 0
+                    if ((self.speed == 1 and self.war_step > 300 and self.war_again)
+                        or (self.speed == 2 and self.war_step > 100 and self.war_again)
+                        or (self.speed == 3 and self.war_step > 2 and self.war_again)):
+                        self.war_step = 0
+                        self.war_again = False
+                    if ((self.speed == 1 and self.war_step > 150 and not self.war_again)
+                        or (self.speed == 2 and self.war_step > 70 and not self.war_again)
+                        or (self.speed == 3 and self.war_step > 2 and not self.war_again)):
+                        self.display_battle = False
+                        self.dislay_war = False
+                        self.collecting_step = 0
+                        self.collecting = True
+                        self.battle = False
+                        self.battle_step = 0
+                        self.war = False
+                        self.war_step = 0
+                        if self.increase_speed:
+                            self.speed = self.increase_speed
+                            self.increase_speed = None
 
         
     def draw(self, game_display, height, width):
@@ -217,6 +269,8 @@ class GameController(object):
 
         if self.dealing_card:
             increment = self.deal_step * 10
+            if self.speed == 2:
+                increment = self.deal_step * 50
             if self.deal_step > 0:
                 game_display.blit(self.cardback_image, (self.deck_pos[0] + i - increment, self.deck_pos[1] - i - increment))
             elif self.deal_step < 0:
@@ -255,6 +309,10 @@ class GameController(object):
             increment = -(self.collecting_step * 9)
             if self.battle_winner == 2:
                 increment = -(increment)
+
+            if self.speed == 2:
+                increment *= 3
+
             game_display.blit(self.player1_battle_cards[-1].image, (self.player1_battle_pos[0], self.player1_battle_pos[1] + increment))      
             game_display.blit(self.player2_battle_cards[-1].image, (self.player2_battle_pos[0], self.player2_battle_pos[1] + increment))      
             if len(self.player1_war_cards) > 0:
@@ -266,12 +324,25 @@ class GameController(object):
                 game_display.blit(self.cardback_image, (self.player2_battle_pos[0] + 225 + i, self.player2_battle_pos[1] + increment))
 
         text = pygame.font.Font('freesansbold.ttf', 20)
+        text_sm = pygame.font.Font('freesansbold.ttf', 12)
         text_surf = text.render(f'Player 1 ({len(self.player1_cards)})', True, (0, 0, 0))
         game_display.blit(text_surf, (5, 5))
-        text_surf = text.render(f'p = pause', True, (0, 0, 0))
-        game_display.blit(text_surf, (5, 30))
-        text_surf = text.render(f'r = reset', True, (0, 0, 0))
-        game_display.blit(text_surf, (5, 60))
+        pausetxt = 'p = pause'
+        if self.paused:
+            pausetxt = 'p = unpause'
+        text_surf = text_sm.render(pausetxt, True, (0, 0, 0))
+        game_display.blit(text_surf, (5, 50))
+        text_surf = text_sm.render('r = reset', True, (0, 0, 0))
+        game_display.blit(text_surf, (5, 70))
+        if self.speed < 3:
+            text_surf = text_sm.render('up = increase speed', True, (0, 0, 0))
+            game_display.blit(text_surf, (5, 90))
+        if self.speed > 1:
+            y = 110
+            if self.speed == 3:
+                y = 90
+            text_surf = text_sm.render('down = decrease speed', True, (0, 0, 0))
+            game_display.blit(text_surf, (5, y))
         text_surf = text.render(f'Player 2 ({len(self.player2_cards)})', True, (0, 0, 0))
         game_display.blit(text_surf, (5, height - 20))
 
